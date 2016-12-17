d0f281e61b376919b9f4e78e2c9fbf32906541ea
Rakesh Kumar <cpp.rakesh@gmail.com>
2016-12-17 21:06:18 +1100

    Test Suite

--- test/src/TestMaths.cpp
+++ test/src/TestMaths.cpp
@@ -1,9 +1,11 @@
 #include "TestMaths.h"
 #include "Common.h"
 #include "Maths.h"
+#include <map>
 #include <cstdio>
 
 namespace ut = utility::test;
+namespace um = utility::maths;
 
 ut::TestMaths::TestMaths() {
     m_initialize_test_cases_dictionary();
@@ -25,5 +27,16 @@ void ut::TestMaths::m_initialize_test_cases_dictionary() {
 }
 
 void ut::TestMaths::m_test_get_continued_fractions() {
+    um::Maths<int> maths;
+    const int N = 45;
+    const int D = 16;
+    std::map<int, int> result = maths.get_continued_fractions(45, 16);
+
+    printf("Continued fraction for [%d] / [%d] is \n", N, D);
+
+    for (std::map<int, int>::const_iterator it = result.begin();
+         it != result.end(); ++it) {
+        printf("[%4d] has [%4d] complete fractions\n", it->first, it->second);
+    }
 }
 

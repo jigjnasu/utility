64ba5fda5c3982124f0270969e2ec33721b2b461
Rakesh Kumar <cpp.rakesh@gmail.com>
2026-03-21 19:04:35 +1100

    refactor

--- kids/testing_system/questions.js
+++ kids/testing_system/questions.js
@@ -24,25 +24,39 @@ const QUIZ_CONFIG = {
 const QUESTIONS = [
   {
     id: 1,
-    question: "Adwita has a gift box with 5 kg chocolates and every chocolate has 5 g weight. She want to make a bag of 20 chocolates.<br>How many total bag she can make?",
+    question: "Adwita has a gift box with 5 kg chocolates and every chocolate has 5 g weight. She want to make a bag of 20 chocolates.\n\nHow many total bag she can make?",
     options: ["40", "50", "60", "70"],
     answer: 1,
     subject: "Mathsematics"
   },
   {
     id: 2,
-    question: "Adwita has a party at 1:00 pm. She spent 55 minutes in prepration of it and another 37 minutes. Cake cooled for another 26 minutes.<br>What is the latest time Adwita should start preparing the cake?",
+    question: "Adwita has a party at 1:00 pm. She spent 55 minutes in prepration of it and another 37 minutes. Cake cooled for another 26 minutes.\n\nWhat is the latest time Adwita should start preparing the cake?",
     options: ["10:52 am", "11:22 am", "11:02 am", "11:52 am"],
     answer: 2,
     subject: "Mathematics"
   },
   {
-    id: 2,
-    question: "Adwita has a party at 1:00 pm. She spent 55 minutes in prepration of it and another 37 minutes. Cake cooled for another 26 minutes.What is the latest time Adwita should start preparing the cake?",
-    options: ["10:52 am", "11:22 am", "11:02 am", "11:52 am"],
-    answer: 2,
+    id: 3,
+    question: "The length of a rectangle is three times its width. If the perimeter of the rectangle is 48 cm.\n\nwhat is the area of the rectangle?",
+    options: ["18", "16", "20", "22"],
+    answer: 0,
     subject: "Mathematics"
   },
+  {
+    id: 4,
+    question: "A car goes at a speed of 40 km/h.\n\nHow far will it travel in 6 minutes?",
+    options: ["6 km", "3 km", "5 km", "4 km"],
+    answer: 3,
+    subject: "Mathematics"
+  },
+  {
+    id: 5,
+    question: "A square has an area of 49 cm<sup>2</sup>. The rectangle has a width that is equal to the side length of this square, and its area is 84 cm<sub>2.</sub>.\n\nWhat is the length of the rectangle?",
+    options: [],
+    answer: 3,
+    subject: "Mathematics"
+  },  
 ];
 
 // Export for use in main app
